feab44c267e3771449508f8b8f5c245035a2af2a
Gabriel <gabriel.abecher@hotmail.com>
2024-08-05 01:03:59 -0300

    added favicon and added correct jason parser

--- routes.js
+++ routes.js
@@ -1,6 +1,3 @@
 const router = require("express").Router();
-const loginController = require("./src/Controllers/loginController");
-
-router.get("/", loginController.index);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,18 +1,32 @@
 require("dotenv").config(); // configura variaveis de ambiente
-
 //imports basicos
 const express = require("express");
+const favicon = require("express-favicon");
 const app = express();
+const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.CONN1)
+  .then(() => {
+    app.emit("ready");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+// diz ao app para ler json=>{}
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 const path = require("path");
-const router = require("./routes.js");
-
+const router = require("./routes");
 // define a pasta de views
+app.use(favicon(__dirname + "/public/images/favicon.ico"));
 app.set("views", path.resolve(__dirname, "src", "Views"));
 app.set("view engine", "ejs");
 // usa o router para as rotas definidas
 app.use(router);
-
 //ouve o backend na porta 3000 padrão
-app.listen(process.env.PORT, () => {
-  console.log(`Link de Acesso: http://localhost:${process.env.PORT}`);
+
+app.on("ready", () => {
+  app.listen(process.env.PORT, () => {
+    console.log(`Link de Acesso: http://localhost:${process.env.PORT}`);
+  });
 });
